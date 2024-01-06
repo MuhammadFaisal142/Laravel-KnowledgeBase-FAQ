013664ba01616016b85ae2fdde1a35a9b0373c38
Muhammad Faisal <muhammadfaisalsaith142@gmail.com>
2024-01-06 12:11:15 +0500

    set five sheets and set search , ascending or descending oder and pagination in this file

--- resources/views/admin/sheets/foreign_population.blade.php
+++ resources/views/admin/sheets/foreign_population.blade.php
@@ -17,7 +17,7 @@
 
     <div class="card-body">
         <div class="table-responsive">
-            <table class=" table table-bordered table-striped table-hover datatable datatable-Permission">
+            <table class="table table-bordered table-striped table-hover datatable datatable-Permission" id="datatable">
                 <thead>
                     <tr>
                         <th width="10">
@@ -74,8 +74,9 @@
                             <td>
                                 {{ $foreignPopulation->Pop_Estr_mulheres ?? '' }}
                             </td>
+                            <td>
 
-
+                            </td>
                         </tr>
                     @endforeach
                 </tbody>
@@ -89,51 +90,9 @@
 @section('scripts')
 @parent
 <script>
-
-
-    $(function () {
-  let dtButtons = $.extend(true, [], $.fn.dataTable.defaults.buttons)
-// @can('permission_delete')
-  let deleteButtonTrans = '{{ trans('global.datatables.delete') }}'
-  let deleteButton = {
-    text: deleteButtonTrans,
-    url: "{{ route('admin.permissions.massDestroy') }}",
-    className: 'btn-danger',
-    action: function (e, dt, node, config) {
-      var ids = $.map(dt.rows({ selected: true }).nodes(), function (entry) {
-          return $(entry).data('entry-id')
-      });
-
-      if (ids.length === 0) {
-        alert('{{ trans('global.datatables.zero_selected') }}')
-
-        return
-      }
-
-      if (confirm('{{ trans('global.areYouSure') }}')) {
-        $.ajax({
-          headers: {'x-csrf-token': _token},
-          method: 'POST',
-          url: config.url,
-          data: { ids: ids, _method: 'DELETE' }})
-          .done(function () { location.reload() })
-      }
-    }
-  }
-  dtButtons.push(deleteButton)
-// @endcan
-
-  $.extend(true, $.fn.dataTable.defaults, {
-    order: [[ 1, 'desc' ]],
-    pageLength: 100,
-  });
-  $('.datatable-Permission:not(.ajaxTable)').DataTable({ buttons: dtButtons })
-    $('a[data-toggle="tab"]').on('shown.bs.tab', function(e){
-        $($.fn.dataTable.tables(true)).DataTable()
-            .columns.adjust();
+    $(document).ready(function () {
+        $('#datatable').DataTable();
     });
-})
-
 </script>
 @endsection
 
--- resources/views/admin/sheets/population_density.blade.php
+++ resources/views/admin/sheets/population_density.blade.php
@@ -16,7 +16,7 @@
 
        <div class="card-body">
            <div class="table-responsive">
-               <table class=" table table-bordered table-striped table-hover datatable datatable-Permission">
+               <table class=" table table-bordered table-striped table-hover datatable datatable-Permission" id="datatable">
                    <thead>
                        <tr>
                            <th width="10">
@@ -61,6 +61,9 @@
                                <td>
                                    {{ $populationDensity->Densidade ?? '' }}
                                </td>
+                               <td>
+
+                               </td>
 
                            </tr>
                        @endforeach
@@ -74,52 +77,14 @@
    @endsection
    @section('scripts')
    @parent
-   <script>
 
 
-       $(function () {
-     let dtButtons = $.extend(true, [], $.fn.dataTable.defaults.buttons)
-   // @can('permission_delete')
-     let deleteButtonTrans = '{{ trans('global.datatables.delete') }}'
-     let deleteButton = {
-       text: deleteButtonTrans,
-       url: "{{ route('admin.permissions.massDestroy') }}",
-       className: 'btn-danger',
-       action: function (e, dt, node, config) {
-         var ids = $.map(dt.rows({ selected: true }).nodes(), function (entry) {
-             return $(entry).data('entry-id')
-         });
 
-         if (ids.length === 0) {
-           alert('{{ trans('global.datatables.zero_selected') }}')
+<script>
+    $(document).ready(function () {
+        $('#datatable').DataTable();
+    });
+</script>
 
-           return
-         }
-
-         if (confirm('{{ trans('global.areYouSure') }}')) {
-           $.ajax({
-             headers: {'x-csrf-token': _token},
-             method: 'POST',
-             url: config.url,
-             data: { ids: ids, _method: 'DELETE' }})
-             .done(function () { location.reload() })
-         }
-       }
-     }
-     dtButtons.push(deleteButton)
-   // @endcan
-
-     $.extend(true, $.fn.dataTable.defaults, {
-       order: [[ 1, 'desc' ]],
-       pageLength: 100,
-     });
-     $('.datatable-Permission:not(.ajaxTable)').DataTable({ buttons: dtButtons })
-       $('a[data-toggle="tab"]').on('shown.bs.tab', function(e){
-           $($.fn.dataTable.tables(true)).DataTable()
-               .columns.adjust();
-       });
-   })
-
-   </script>
    @endsection
 
